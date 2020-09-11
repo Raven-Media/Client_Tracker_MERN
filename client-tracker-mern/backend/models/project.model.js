3fbe421412a498cb8e5e6c802c146b64f6dd569a
Ian Edwards <ian.edward@uconn.edu>
2020-09-11 14:19:44 -0400

    Update project.model.js
    Created basic layout of Project Schema

--- client-tracker-mern/backend/models/project.model.js
+++ client-tracker-mern/backend/models/project.model.js
@@ -2,3 +2,18 @@ const mongoose = require('mongoose');
 
 const Schema = mongoose.Schema; 
 
+const projectSchema = new Schema({
+    project_name: { type: String, required: true },
+    project_description: { type: String, required: true },
+    project_stack: { type: String, required: true },
+    project_phases: { type: Number, required: true },
+    estimated_duration: { type: Number, required: true },
+    start_date: { type: Date, required: true },
+    
+}, {
+    timestamps: true,
+}); 
+
+const Project = mongoose.model('Project', projectSchema); 
+
+module.exports = Project; 
