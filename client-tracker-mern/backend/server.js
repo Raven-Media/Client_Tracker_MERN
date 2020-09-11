5fa43469954fe563a58dde5386038c5a8120af89
Ian Edwards <ian.edward@uconn.edu>
2020-09-11 13:29:18 -0400

    Update
    Added "mongoose.set('useUnifiedTopology', true);" to server.js
    Added project and client models to backend folder

--- client-tracker-mern/backend/server.js
+++ client-tracker-mern/backend/server.js
@@ -10,8 +10,11 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+mongoose.set('useUnifiedTopology', true);
+
 const uri = process.env.ATLAS_URI;
 mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true });
+
 
 const connection = mongoose.connection;
 connection.once('open', () => {
