3c87711657f475fd04d3c21245f709af8141d112
Ian Edwards <ian.edward@uconn.edu>
2020-10-02 01:57:31 -0400

    Update projects.js
    Correct syntax implemnted for const newProject
    removed superflous code and documented at bottom of file

--- client-tracker-mern/backend/routes/projects.js
+++ client-tracker-mern/backend/routes/projects.js
@@ -7,21 +7,17 @@ router.route('/').get((req, res) => {
         .catch(err => res.status(400).json('Error: ' + err));
 });
 
-router.route('/add').post((req, res) => {
-    const projectName = req.body.project_name
-    const projectDescription = req.body.project_description
-    const projectStack = req.body.project_stack
-    const projectPhases = Number(req.body.project_phases)
-    const estDuration = Number(req.body.estimated_duration)
-    const startDate = Date.parse(req.body.start_date)
+// router.route('/add').post((req, res) => {
+router.post("/add", (req, res) => {
 
     const newProject = new Project({
-        projectName,
-        projectDescription,
-        projectStack,
-        projectPhases,
-        estDuration,
-        startDate
+        project_name: req.body.project_name,
+        project_description: req.body.project_description,
+        project_stack: req.body.project_stack,
+        project_phases: Number(req.body.project_phases),
+        estimated_duration: Number(req.body.estimated_duration),
+        start_date: Date.parse(req.body.start_date)
+
     });
 
     newProject.save()
@@ -29,4 +25,31 @@ router.route('/add').post((req, res) => {
         .catch(err => res.status(400).json('Error: ' + err));
 });
 
-module.exports = router; 
+module.exports = router;
+
+//Misc variable declarations 
+
+//     const projectName = req.body.project_name
+//     const projectDescription = req.body.project_description
+//     const projectStack = req.body.project_stack
+//     const projectPhases = Number(req.body.project_phases)
+//     const estDuration = Number(req.body.estimated_duration)
+//     const startDate = Date.parse(req.body.start_date)
+
+// projectName,
+// projectDescription,
+// projectStack,
+// projectPhases,
+// estDuration,
+// startDate
+
+
+//Insomnia/Postman Model Route Test JSON 
+// {
+// "project_name": "Test # 1",
+// "project_description": "String",
+// "project_stack": "String",
+// "project_phases": "1",
+// "estimated_duration": "1",
+// "start_date": "05-27-2020"
+// }
